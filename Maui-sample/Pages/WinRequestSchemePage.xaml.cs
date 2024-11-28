cae690ab662f6682ee93f218b0027261de966505
AP\tchen <terence_chen@trimble.com>
2024-11-28 16:30:12 +1300

    slightly changed response string. Added cs file under platform/windows directory

--- Maui-sample/Pages/WinRequestSchemePage.xaml.cs
+++ Maui-sample/Pages/WinRequestSchemePage.xaml.cs
@@ -1,15 +1,14 @@
 using System;
 using System.Diagnostics;
-//using Windows.System;
 
 namespace Maui_sample;
 
 public partial class WinRequestSchemePage : ContentPage
 {
-	public WinRequestSchemePage()
-	{
-		InitializeComponent();
-	}
+  public WinRequestSchemePage()
+  {
+    InitializeComponent();
+  }
   private async Task<string> SampleAppID()
   {
     string appID = await SecureStorage.Default.GetAsync("sampleAppID");
@@ -22,16 +21,23 @@ public partial class WinRequestSchemePage : ContentPage
 
   public async void Click_Register_Button(object sender, EventArgs e)
   {
-    string sampleAppID = await SampleAppID();
-    string callbackUri = "sampleapp://response/tmmRegister";
-    Uri.EscapeDataString(callbackUri);
-    string requestString = $"trimbleMobileManager://request/tmmRegister?applicationId={sampleAppID}&callback={callbackUri}";
-    Uri requestUri = new Uri(requestString);
-    Debug.WriteLine(requestUri);
-
-    if (await Launcher.CanOpenAsync(requestUri))
+    try
     {
-      bool result = await Launcher.OpenAsync(requestUri);
+      string sampleAppID = await SampleAppID();
+      string callbackUri = "sampleapp://response";
+      Uri.EscapeDataString(callbackUri);
+      string requestString = $"trimbleMobileManager://request/tmmRegister?applicationId={sampleAppID}&callback={callbackUri}";
+      Uri requestUri = new Uri(requestString);
+      Debug.WriteLine(requestUri);
+
+      if (await Launcher.CanOpenAsync(requestUri))
+      {
+        bool result = await Launcher.OpenAsync(requestUri);
+      }
+    }
+    catch (Exception ex)
+    {
+      Debug.WriteLine(ex.Message);
     }
   }
 }
