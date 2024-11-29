65b92d342fed658868903097cfd3db94e823adb5
AP\tchen <terence_chen@trimble.com>
2024-11-29 16:20:04 +1300

    Change console.writeline to debug.writeline

--- Maui-sample/Platforms/Windows/WinRequestSchemeService.cs
+++ Maui-sample/Platforms/Windows/WinRequestSchemeService.cs
@@ -8,6 +8,7 @@ using Launcher = Windows.System.Launcher;
 // Ambiguity of the Launcher class requires the namespace to be specified
 using Newtonsoft.Json.Linq;
 using Maui_sample;
+using System.Diagnostics;
 
 namespace Maui_sample.Platforms.Windows
 {
@@ -31,7 +32,7 @@ namespace Maui_sample.Platforms.Windows
         if (supportsRequest)
         {
           bool result = await Launcher.LaunchUriAsync(requestToLaunch);
-          Console.WriteLine(result);
+          Debug.WriteLine(result);
 
           if (returnCallback != null)
           {
@@ -40,12 +41,12 @@ namespace Maui_sample.Platforms.Windows
         }
         else
         {
-          Console.WriteLine("Request not supported.");
+          Debug.WriteLine("Request not supported.");
         }
       }
       catch (Exception ex)
       {
-        Console.WriteLine("Error in CallAsync: " + ex.Message);
+        Debug.WriteLine("Error in CallAsync: " + ex.Message);
       }
     }
 
@@ -58,7 +59,7 @@ namespace Maui_sample.Platforms.Windows
       }
       else
       {
-        Console.WriteLine("No WinRequestSchemePage instance available to handle the response.");
+        Debug.WriteLine("No WinRequestSchemePage instance available to handle the response.");
       }
     }
 
