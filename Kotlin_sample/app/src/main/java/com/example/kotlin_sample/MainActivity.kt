612b81c173148df28e6289aeeb5fa2201d54c0d3
AP\tchen <terence_chen@trimble.com>
2024-12-04 10:10:25 +1300

    Gets version number.

--- Kotlin_sample/app/src/main/java/com/example/kotlin_sample/MainActivity.kt
+++ Kotlin_sample/app/src/main/java/com/example/kotlin_sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kotlin_sample
 
 import android.os.Bundle
 import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-//        CheckBox ()
-
+//      Version
+      val versionText: TextView = findViewById(R.id.versionText)
+      var versionNumber = applicationContext.packageManager.getPackageInfo(applicationContext.packageName, 0).versionName
+      versionText.text = getString(R.string.version, versionNumber)
+//        CheckBox
+      val checkB: CheckBox = findViewById(R.id.customIDTextCheckBox)
+      val tileText: TextInputLayout = findViewById(R.id.appIDTextTitle)
+      val appIDInput: TextInputEditText = findViewById(R.id.appIDEditText)
+//      checkB.setOnClickListener{
+//        if (checkB.isChecked)
+//          tileText.visibility
+//      }
     }
 }
