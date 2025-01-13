83b25fc269375ec6bc3157aba64873aae12b07c9
AP\tchen <terence_chen@trimble.com>
2025-01-14 09:58:22 +1300

    Initialize swift project.

---
+++ swift_sample/swift_sample/swift_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sampleApp.swift
+//  swift_sample
+//
+//  Created by kea-build on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
