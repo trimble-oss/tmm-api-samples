37089062c91ebf94b3db91ea4fc97c1dfcda80fb
AM\DColvin <daniel_colvin@trimble.com>
2026-08-18 17:19:44 -0700

    fix: force Bouncy Castle 1.85 to resolve Mend CVE alerts on lint-gradle

--- android-native-sample/build.gradle.kts
+++ android-native-sample/build.gradle.kts
@@ -8,8 +8,9 @@ plugins {
 subprojects {
     configurations.configureEach {
         resolutionStrategy {
-            force("org.bouncycastle:bcprov-jdk18on:1.84")
-            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bouncycastle:bcutil-jdk18on:1.85")
         }
     }
 }
