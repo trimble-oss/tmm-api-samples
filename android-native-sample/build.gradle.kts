9cc9b9bbef52369637bf9247d68d5da2e966d3d4
AM\DColvin <daniel_colvin@trimble.com>
2026-08-24 16:25:47 -0700

    Fixed CVE-2025-48924 and CVE-2020-13956

--- android-native-sample/build.gradle.kts
+++ android-native-sample/build.gradle.kts
@@ -11,6 +11,10 @@ subprojects {
             force("org.bouncycastle:bcprov-jdk18on:1.85")
             force("org.bouncycastle:bcpkix-jdk18on:1.85")
             force("org.bouncycastle:bcutil-jdk18on:1.85")
+            // CVE-2025-48924 (lint-gradle -> commons-lang3 3.16.0)
+            force("org.apache.commons:commons-lang3:3.18.0")
+            // CVE-2020-13956 (lint-gradle -> httpclient 4.5.6)
+            force("org.apache.httpcomponents:httpclient:4.5.14")
         }
     }
 }
